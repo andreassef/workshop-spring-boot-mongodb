87bc63cda0aca1a5c5ae7d69605812a1e5d738fb
Andre Assef <andreassef02@gmail.com>
2019-12-31 18:56:39 -0400

    Operacao de Instanciacao de base de dados

---
+++ src/main/java/com/andreassef/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.andreassef.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.andreassef.workshopmongo.domain.User;
+import com.andreassef.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
