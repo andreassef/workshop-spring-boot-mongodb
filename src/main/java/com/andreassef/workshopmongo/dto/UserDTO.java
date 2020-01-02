930f14509e1f15006d9ba479034a0b777dc911b7
Andre Assef <andreassef02@gmail.com>
2020-01-02 14:39:31 -0400

    Insercao de usuario com Post

--- src/main/java/com/andreassef/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/andreassef/workshopmongo/dto/UserDTO.java
@@ -12,7 +12,6 @@ public class UserDTO implements Serializable {
 	private String email;
 	
 	public UserDTO(){
-		
 	}
 	
 	public UserDTO(User obj) {
@@ -44,6 +43,4 @@ public class UserDTO implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
 }
